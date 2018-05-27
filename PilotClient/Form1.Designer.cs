1bdb7c1cf9a05a84e96bb70195ea50620ba64ff6
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-05-27 11:14:06 +0100

    a bit more managed

---
+++ PilotClient/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace PilotClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 12);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(253, 389);
+            this.txtLog.TabIndex = 0;
+            this.txtLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 413);
+            this.Controls.Add(this.txtLog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.OnSimConnectEvent += new SimLib.SimConnectForm.SimConnectEvent(this.Form1_OnSimConnectEvent);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtLog;
+    }
+}
+
---
+++ PilotClient/Form1.cs
@@ -0,0 +1,70 @@
+﻿using SimLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PilotClient
+{
+    public partial class Form1 : SimConnectForm
+    {
+        // Response number 
+        int response = 1;
+
+        // Output text - display a maximum of 10 lines 
+        string output = "\n\n\n\n\n\n\n\n\n\n";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_OnSimConnectEvent(Microsoft.FlightSimulator.SimConnect.SimConnect sender, Microsoft.FlightSimulator.SimConnect.SIMCONNECT_RECV_EVENT recEvent)
+        {
+            switch (recEvent.uEventID)
+            {
+                case (uint)EVENTS.PITOT_TOGGLE:
+
+                    displayText("PITOT switched");
+                    break;
+
+                case (uint)EVENTS.FLAPS_UP:
+
+                    displayText("Flaps Up");
+                    break;
+
+                case (uint)EVENTS.FLAPS_DOWN:
+
+                    displayText("Flaps Down");
+                    break;
+
+                case (uint)EVENTS.FLAPS_INC:
+
+                    displayText("Flaps Inc");
+                    break;
+
+                case (uint)EVENTS.FLAPS_DEC:
+
+                    displayText("Flaps Dec");
+                    break;
+            }
+        }
+
+        void displayText(string s)
+        {
+            // remove first string from output 
+            output = output.Substring(output.IndexOf("\n") + 1);
+
+            // add the new string 
+            output += "\n" + response++ + ": " + s;
+
+            // display it 
+            txtLog.Text = output;
+        }
+    }
+}
